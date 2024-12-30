3cf8adcf68864146cbcf893ac1401f2d8efffdf3
Tazriyan Sirat <li9vyrit@students.codeinstitute.net>
2024-12-30 18:46:30 +0000

    Create instructions at the start of the game

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 import random
 
+def instructions():
+    print("It's the year 5056. You race against the AI to see who reaches space 25 first!")
+    print("You take turns & roll 1-6, the number corresponding the spaces you move.")
+    print("But beaware! There are space obstacles on some spaces that hinders your progress.")
+    print("The obstacles & their hindrances are:")
+    print("Asteroid Impact = -1 \nGamma ray Blast = -3 \nSupernova Pull = -5 \nNeutron Star Collision = Start over!")
+    print("Best of luck Racer!")
+
 def create_board():
     """
     Creates a board for the race that contains a list of 25 spaces 
@@ -7,13 +15,18 @@ def create_board():
     hinder progress in the race.
     """
     board = [None] * 25
-    board[4] = "asteroid_impact"
-    board[7] = "gamma_ray_burst"
-    board[8] = "supernova_pull"
-    board[12] = "asteroid_impact"
-    board[16] = "supernova_pull"
-    board[20] = "gamma_ray_burst"
-    board[23] = "blackhole_impact"
-    return board
+    board[4] = "move_back_1"
+    board[7] = "move_back_3"
+    board[8] = "move_back_5"
+    board[12] = "move_back_1"
+    board[15] = "move_back_5"
+    board[18] = "move_back_3"
+    board[20] = "move_back_1"
+    board[23] = "start_over"
+    print(board)
 
+def main():
+    instructions()
+    create_board()
 
+main()
